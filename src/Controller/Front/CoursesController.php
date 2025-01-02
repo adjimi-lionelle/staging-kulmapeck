967e5c5854f2f4aba535c2c88379cf5834317540
adjimi-lionelle <lionelleadjimi@gmail.com>
2025-01-03 00:24:00 +0100

    ajout de la fonctionnalité de la gratuité des cores pendant 2 semaies

--- src/Controller/Front/CoursesController.php
+++ src/Controller/Front/CoursesController.php
@@ -433,13 +433,13 @@ class CoursesController extends AbstractController
             $dateInscription = $eleve->getJoinAt(); // Utilisez le champ existant `join_at`
             $dateFinGratuite = (clone $dateInscription)->modify('+2 weeks'); // Ajoutez 2 semaines à la date d'inscription
             $periodeGratuiteActive = new \DateTime() <= $dateFinGratuite; // Vérifiez si la période gratuite est active
-            var_dump($periodeGratuiteActive); die();
+            //var_dump($periodeGratuiteActive); die();
 
             // On verifie si l'élève n'a pas déjà ce cours dans sa liste des cours
             // le cours soit souscrire a un compte premium
 
-            //if (!$course->isIsFree() && (!$eleve->isIsPremium() && (!$paymentRepository->findOneBy(['eleve' => $eleve, 'cours' => $course, 'isExpired' => false])))) {
-            if ( !$eleve->isIsPremium() && (!$paymentRepository->findOneBy(['eleve' => $eleve, 'cours' => $course, 'isExpired' => false]))) {
+            if (!$course->isIsFree() && (!$eleve->isIsPremium() && (!$paymentRepository->findOneBy(['eleve' => $eleve, 'cours' => $course, 'isExpired' => false])))) {
+           // if ( !$eleve->isIsPremium() && (!$paymentRepository->findOneBy(['eleve' => $eleve, 'cours' => $course, 'isExpired' => false]))) {
                 return $this->redirectToRoute('app_front_payment_buy_course', ['slug' => $course->getSlug()]);
             }
             if (!$eleve->getCours()->contains($course)) {
