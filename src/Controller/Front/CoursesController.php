a9ed9a660d29f3f1f49ddeccf265e419123b916a
adjimi-lionelle <lionelleadjimi@gmail.com>
2025-01-03 00:07:17 +0100

    ajout de la fonctionnalité de la gratuité des cores pendant 2 semaies

--- src/Controller/Front/CoursesController.php
+++ src/Controller/Front/CoursesController.php
@@ -431,9 +431,9 @@ class CoursesController extends AbstractController
 
             // Vérification de la période de gratuité
             $dateInscription = $eleve->getJoinAt(); // Utilisez le champ existant `join_at`
-            $dateFinGratuite = (clone $dateInscription)->modify('+2 months +2 weeks'); // Ajoutez 2 semaines à la date d'inscription
+            $dateFinGratuite = (clone $dateInscription)->modify('+2 weeks'); // Ajoutez 2 semaines à la date d'inscription
             $periodeGratuiteActive = new \DateTime() <= $dateFinGratuite; // Vérifiez si la période gratuite est active
-            var_dump($periodeGratuiteActive); die();
+            //var_dump($periodeGratuiteActive); die();
 
             // On verifie si l'élève n'a pas déjà ce cours dans sa liste des cours
             // le cours soit souscrire a un compte premium
