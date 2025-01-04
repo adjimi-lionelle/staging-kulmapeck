234f7ea70c0d9bb5505ee9bc788ce75a8e5bc60e
HIS-MAJESTY-KING-DAVID <hismajestykingdavid@gmail.com>
2025-01-04 14:21:49 +0100

    fix: Update RegistrationController with proper Personne entity namespace and simplified registration flow

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,34 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Personne;
 use App\Form\SimpleRegistrationType;
 use App\Repository\NetworkConfigRepository;
 use App\Repository\PersonneRepository;
 use App\Repository\UserRepository;
 use App\Service\FileUploader;
+use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
 {
+    private EmailVerifier $emailVerifier;
 
-    public function __construct()
+    public function __construct(EmailVerifier $emailVerifier)
     {
+        $this->emailVerifier = $emailVerifier;
     }
 
-    #[Route('/register', name: 'app_front_register')]
-
+    #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, FileUploader $fileUploader, UserRepository $userRepository, NetworkConfigRepository $networkConfigRepository, PersonneRepository $personneRepository): Response
     {
         $user = new User();
-        if ($request->query->get('invitation')) {
-            $user->parentCode = $request->query->get('invitation');
-        }
-
         $form = $this->createForm(SimpleRegistrationType::class, $user);
         $form->handleRequest($request);
 
@@ -45,7 +46,7 @@ class RegistrationController extends AbstractController
 
             // Set the user as verified immediately
             $user->setIsVerified(true);
-            
+
             // Create and set Personne entity
             $personne = new Personne();
             $personne->setNomComplet($form->get('fullName')->getData());
@@ -53,23 +54,25 @@ class RegistrationController extends AbstractController
             $user->setPersonne($personne);
 
             if ($form->get('parentCode')->getData()) {
-                $personne->setParent($personneRepository->findOneBy(['invitationCode' => $form->get('parentCode')->getData()]));
+                $parentUser = $userRepository->findOneBy(['invitationCode' => $form->get('parentCode')->getData()]);
+                if ($parentUser) {
+                    $personne->setParent($personneRepository->findOneBy(['invitationCode' => $form->get('parentCode')->getData()]));
+                }
             }
 
             // Generate invitation code
-            $codeInvitation = self::generateInvitationCode($personneRepository);
-            $invitationLink = json_encode([
-                'trainer' => 'https://kulmapeck.com' . $this->generateUrl('app_front_register', ['type' => 'trainer', 'invitation' => $codeInvitation]),
-                'student' => 'https://kulmapeck.com' . $this->generateUrl('app_front_register', ['type' => 'student', 'invitation' => $codeInvitation])
+            $codeInvitation = $this->generateInvitationCode($personneRepository);
+            $invitationLink = $request->getSchemeAndHttpHost() . $this->generateUrl('app_register', [
+                'code' => $codeInvitation
             ]);
-            
+
             $personne->setInvitationCode($codeInvitation)
                     ->setInvitationLink($invitationLink);
 
+            $entityManager->persist($personne);
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // Automatically authenticate the user
             return $this->redirectToRoute('app_login');
         }
 
@@ -78,53 +81,31 @@ class RegistrationController extends AbstractController
         ]);
     }
 
-    #[Route('/registration/success', name: 'app_registration_success')]
-
-    public function confirmation(Request $request)
+    private function generateInvitationCode(PersonneRepository $pr): string
     {
-        return $this->render('registration/success.html.twig', [ 'activatedConfirmation' => $request->query->get('activated') ] );
-    }
-
-    private function uploadImagesFiles(User $user, FileUploader $fileUploader, $path = 'images/enseignants/kyc')
-    {
-
-        // dd( $user->getPersonne() );
-        if ($user->getPersonne()->getImageFile() != null ) {
-
-            $fileName = $fileUploader->upload($user->getPersonne()->getImageFile(), $path);
-            $user->getPersonne()->setAvatar($fileName);
+        $code = substr(str_shuffle(str_repeat('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 6)), 0, 6);
+        while ($pr->findOneBy(['invitationCode' => $code])) {
+            $code = substr(str_shuffle(str_repeat('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 6)), 0, 6);
         }
-
-        if ($user->getEnseignant() !== null ) {
-            $user->getEnseignant()->setDiplome($fileUploader->upload($user->getEnseignant()->diplomeFile, $path))
-            ->setRectoCNI($fileUploader->upload($user->getEnseignant()->rectoCNIFile, $path))
-            ->setVersoCNI($fileUploader->upload($user->getEnseignant()->versoCNIFile, $path))
-            ->setSelfieCNI($fileUploader->upload($user->getEnseignant()->selfieCNIFile, $path))
-            ->setEmploiDuTemps($fileUploader->upload($user->getEnseignant()->emploiDuTempsFile, $path));
-            $user->setRoles(['ROLE_INSTRUCTOR']);
-        } elseif ($user->getEleve() !== null ) {
-            $user->setRoles(['ROLE_STUDENT']);
-        }
-
-        return $user;
-    }
-
-    public static function generateInvitationCode(PersonneRepository $pr)
-    {
-        $tab = ['A', 'Z', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'Q', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'W', 'X', 'C', 'V', 'B', 'N', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-        $exist = true;
-        $code = '';
-        while ($exist) {
-            $code = '';
-            for ($i = 0; $i < 8; $i++) {
-                $code .= $tab[rand(0, count($tab) - 1)];
-            }
-            $p = $pr->findOneBy(['invitationCode' => $code]);
-            if (is_null($p)) {
-                $exist = false;
-            }
-        }
-
         return $code;
+    }
+
+    #[Route('/verify/email', name: 'app_verify_email')]
+    public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // validate email confirmation link, sets User::isVerified=true and persists
+        try {
+            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+
+            return $this->redirectToRoute('app_register');
+        }
+
+        $this->addFlash('success', 'Your email address has been verified.');
+
+        return $this->redirectToRoute('app_home');
     }
 }
