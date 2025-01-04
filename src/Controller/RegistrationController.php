d68a47365160bd5fe3ad2f4041fe0a3d779b0f78
HIS-MAJESTY-KING-DAVID <hismajestykingdavid@gmail.com>
2025-01-04 14:51:41 +0100

    fix: Update registration route name to app_front_register consistently

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -28,7 +28,7 @@ class RegistrationController extends AbstractController
         $this->emailVerifier = $emailVerifier;
     }
 
-    #[Route('/register', name: 'app_register')]
+    #[Route('/register', name: 'app_front_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, FileUploader $fileUploader, UserRepository $userRepository, NetworkConfigRepository $networkConfigRepository, PersonneRepository $personneRepository): Response
     {
         $user = new User();
@@ -62,7 +62,7 @@ class RegistrationController extends AbstractController
 
             // Generate invitation code
             $codeInvitation = $this->generateInvitationCode($personneRepository);
-            $invitationLink = $request->getSchemeAndHttpHost() . $this->generateUrl('app_register', [
+            $invitationLink = $request->getSchemeAndHttpHost() . $this->generateUrl('app_front_register', [
                 'code' => $codeInvitation
             ]);
 
@@ -73,7 +73,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('app_front_register');
         }
 
         return $this->render('registration/register_simple.html.twig', [
@@ -101,11 +101,11 @@ class RegistrationController extends AbstractController
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_front_register');
         }
 
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_home');
+        return $this->redirectToRoute('app_front_register');
     }
 }
