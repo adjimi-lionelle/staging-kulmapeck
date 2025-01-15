eb713b587b34cfc97a2d6fda30d076d3a4c4a343
HIS-MAJESTY-KING-DAVID <hismajestykingdavid@gmail.com>
2025-01-15 20:36:52 +0100

    feat(i18n): update form validation messages
    - Add translation keys for all form validation messages
    - Update form constraints to use translation keys
    - Add French translations for validation messages
    - Improve validation rules for username and phone number
    - Add specific error messages for each validation rule

--- src/Form/SimpleRegistrationType.php
+++ src/Form/SimpleRegistrationType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
@@ -23,6 +25,12 @@ class SimpleRegistrationType extends AbstractType
                 'mapped' => false,
                 'required' => false,
                 'label' => 'Code d\'invitation',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[A-Z0-9]{8}$/',
+                        'message' => 'INVALID_INVITATION_CODE_KEY'
+                    ])
+                ]
             ])
             ->add('fullName', TextType::class, [
                 'mapped' => false,
@@ -33,9 +41,9 @@ class SimpleRegistrationType extends AbstractType
                 ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Veuillez entrer votre nom complet',
-                    ]),
-                ],
+                        'message' => 'REQUIRED_FIELD_KEY'
+                    ])
+                ]
             ])
             ->add('username', TextType::class, [
                 'mapped' => true,
@@ -46,15 +54,17 @@ class SimpleRegistrationType extends AbstractType
                 ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Veuillez entrer un nom d\'utilisateur',
+                        'message' => 'REQUIRED_FIELD_KEY'
                     ]),
-                    new Length([
-                        'min' => 5,
-                        'max' => 8,
-                        'minMessage' => 'Le nom d\'utilisateur doit faire au moins {{ limit }} caractères',
-                        'maxMessage' => 'Le nom d\'utilisateur ne peut pas dépasser {{ limit }} caractères',
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9_]+$/',
+                        'message' => 'INVALID_USERNAME_KEY'
                     ]),
-                ],
+                    new Callback([
+                        'callback' => [$this, 'validateUniqueUsername'],
+                        'message' => 'USERNAME_EXISTS_KEY'
+                    ])
+                ]
             ])
             ->add('phoneNumber', TextType::class, [
                 'required' => true,
@@ -63,19 +73,17 @@ class SimpleRegistrationType extends AbstractType
                 ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Veuillez entrer votre numéro de téléphone',
-                    ]),
-                    new Length([
-                        'min' => 8,
-                        'max' => 15,
-                        'minMessage' => 'Le numéro de téléphone doit faire au moins {{ limit }} caractères',
-                        'maxMessage' => 'Le numéro de téléphone ne peut pas dépasser {{ limit }} caractères',
+                        'message' => 'REQUIRED_FIELD_KEY'
                     ]),
                     new Regex([
-                        'pattern' => '/^[0-9\+]+$/',
-                        'message' => 'Le numéro de téléphone ne doit contenir que des chiffres et le signe +',
+                        'pattern' => '/^\+?[0-9]{8,15}$/',
+                        'message' => 'INVALID_PHONE_KEY'
                     ]),
-                ],
+                    new Callback([
+                        'callback' => [$this, 'validateUniquePhone'],
+                        'message' => 'PHONE_EXISTS_KEY'
+                    ])
+                ]
             ])
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
@@ -85,25 +93,32 @@ class SimpleRegistrationType extends AbstractType
                 ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Veuillez entrer un mot de passe',
+                        'message' => 'REQUIRED_FIELD_KEY'
                     ]),
                     new Length([
                         'min' => 8,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
-                        'max' => 4096,
+                        'minMessage' => 'MIN_8_CHARS_KEY'
                     ]),
                     new Regex([
-                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/',
-                        'message' => 'Votre mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule et un chiffre',
+                        'pattern' => '/[A-Z]/',
+                        'message' => 'UPPERCASE_REQUIRED_KEY'
                     ]),
-                ],
+                    new Regex([
+                        'pattern' => '/[a-z]/',
+                        'message' => 'LOWERCASE_REQUIRED_KEY'
+                    ]),
+                    new Regex([
+                        'pattern' => '/[0-9]/',
+                        'message' => 'NUMBER_REQUIRED_KEY'
+                    ])
+                ]
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+                    new IsTrue([
+                        'message' => 'TERMS_REQUIRED_KEY'
+                    ])
                 ],
                 'label_html' => true,
                 'label' => 'By signing up, you agree to the <a href="">terms</a>'
