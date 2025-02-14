2290025b30483436bc749773eece5f40833a49fc
HIS-MAJESTY-KING-DAVID <hismajestykingdavid@gmail.com>
2025-02-14 15:45:53 +0100

    Controller checks if student needs setup by checking both classe and specialite\nSets needsSetup flag accordingly\nPasses this flag to the template\nTemplate uses this flag to decide whether to show setup modal or chat interface\n

--- src/Controller/GroupChatController.php
+++ src/Controller/GroupChatController.php
@@ -230,6 +230,7 @@ class GroupChatController extends AbstractController
             $groups = $this->groupChatRepository->findByStudent($eleve);
         }
 
+        // Pass needsSetup flag to template
         return $this->render('front/chat/index.html.twig', [
             'eleve' => $eleve,
             'classes' => $classes,
